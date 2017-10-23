6d8f858139ffa771d3d38dd56bbada269c65f549
MargoJane <31864917+MargoJane@users.noreply.github.com>
2017-10-23 16:08:30 -0400

    Add files via upload

---
+++ .gitignore/cubeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+
+	void OnMouseDown(){
+		gameObject.GetComponent<Renderer> ().material.color = Color.red;
+
+		if (gameController.selectedCube != null) {
+			gameController.selectedCube.GetComponent<Renderer> ().material.color = Color.white;
+		}
+		gameController.selectedCube = gameObject;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
+
---
+++ .gitignore/gameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+	public static Vector3 cubePosition;
+	public static float xPosition;
+	public static int numCubes;
+
+	public static GameObject myCube;
+	public GameObject cubePrefab;
+
+	public static GameObject selectedCube;
+
+	// Use this for initialization
+	void Start () {
+		numCubes = 16;
+		xPosition = -21f;
+
+		for (int i = 0; i < numCubes; i++) {
+			cubePosition = new Vector3 (xPosition, 0, 0);
+			myCube = Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+			myCube.GetComponent<Renderer>().material.color = Color.white;
+			xPosition += 3;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update (){
+		}
+
+}
